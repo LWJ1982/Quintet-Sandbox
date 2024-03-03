a53015d29171b7aa9b59183115935f2f31dfaa4c
LWJ1982 <iambornin1982@gmail.com>
2024-03-04 01:30:36 +0800

    del src

--- src/waf.tf
+++
@@ -1,7 +0,0 @@
-# creating aws-waf-cloudfront
-
-module "waf-webaclv2" {
-  source      = "umotif-public/waf-webaclv2/aws"
-  version     = "5.1.2"
-  name_prefix = "dynamics-waf-cf_dist" # insert the 1 required variable here
-}
