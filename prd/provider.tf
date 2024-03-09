f81cfac0ac324f295b9a921e57d001d13a249055
Muhammad Rudyn <nydurism@gmail.com>
2024-03-09 11:49:46 +0800

    Update provider.tf

--- prd/provider.tf
+++ prd/provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-# provider "aws" {
-#   region = "us-east-1"
-#   alias  = "us-east-1"
-# }
+provider "aws" {
+  region = "us-east-1"
+  alias  = "us-east-1"
+}
